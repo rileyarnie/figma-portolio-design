21baee6611bbfc78d0508f5710a470d291ad5b40
Arnold Kibira <arnold.ril3y@gmail.com>
2021-05-17 22:05:04 +0300

    render all components to to the specified mobile design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Works from "./pages/Works";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div className="">
-      start
+    <div className="grid grid-cols-1">
+      <Navbar />
+      <Home />
+      <Blogs />
+      <Works />
+      <Footer />
     </div>
   );
 }
