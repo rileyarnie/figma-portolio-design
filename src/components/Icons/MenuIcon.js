61160555d74cf99746ab67fae25c6d0e5ea0f431
Arnold Kibira <arnold.ril3y@gmail.com>
2021-05-17 21:53:20 +0300

    add menu item icno

---
+++ src/components/Icons/MenuIcon.js
@@ -0,0 +1,10 @@
+const MenuIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-7.5 w-7.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+    )
+}
+
+export default MenuIcon
+
