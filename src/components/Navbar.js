69a23df807f3f2075a2280d994b218af7c835507
Arnold Kibira <arnold.ril3y@gmail.com>
2021-05-17 21:52:58 +0300

    create mobile navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import MenuIcon from "./Icons/MenuIcon"
+
+const Navbar = () => {
+
+    return (
+        <nav className="bg-white shadow-md pt-5 px-2 h-ten flex justify-end sticky top-0 z-10">
+            <MenuIcon />
+
+        </nav>
+    )
+}
+
+export default Navbar
